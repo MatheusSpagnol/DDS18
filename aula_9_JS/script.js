4d6e8d04a865acec3d69350b8a6c37d0d729d12f
Matheus <Matheus.spagnol@aluno.senai.br>
2024-08-21 16:59:44 -0300

    aula 9

---
+++ aula_9_JS/script.js
@@ -0,0 +1,104 @@
+//alert("outra mensagem")
+/*
+console.log("Olha a mensagem")
+
+
+var texto = "Torre Gêmeas"
+var num = 2001
+var theBest = true
+var nulo = null 
+var indefinido 
+
+
+
+console.log(typeof(texto))
+console.log(typeof(texto)) 
+console.log(typeof(num)) 
+console.log(typeof(theBest)) 
+console.log(typeof(null)) 
+console.log(typeof(indefinido))     
+*/
+
+//Operadores
+
+//Aritméticos:+-/%
+
+// var a = 10
+// var b = 3
+
+// console.log("Soma",a+b)
+// console.log("Subtração",a-b)
+// console.log("Multiplicação",a*b)
+// console.log("Divisão", a/b)
+// console.log("Módulo", a%b)
+
+//Lógicos: &&, ||, !
+// ! = Não
+// && = And
+// || = ou
+
+// var verdade = true
+// var  mentira = false
+
+// console.log( verdade && mentira)
+// console.log( verdade || mentira)
+// console.log( verdade && !mentira)
+
+ //Relacionais: >, <, >=,==!=, ===, !==
+
+// var a = 10
+// var b = 5
+// var c = "10"
+
+
+//  console.log( "Relacionais")
+//  console.log(a > b)
+//  console.log(a < b)
+//  console.log(a >= b)
+//  console.log(a <= b)
+//  console.log(a == b)
+//  console.log(a != b)
+//  console.log(a === b)
+//  console.log(a !== b)
+
+
+// var estaVivo = true 
+// if(estaVivo){
+//     console.log("Esá vivo")
+// }
+
+let num1 = parseFloat(prompt("Digite o num 1: "))
+let num2 = parseFloat(prompt("Digite o num 2: "))
+let result = num1+num2
+
+console.log("Resultado: " + result)
+
+alert("Resultado: " + result)
+
+
+
+//loops
+//For
+// for(var i = 0; i < 5; i++){
+//     console.log("Estoy ahi");
+// }
+//     console.log("Acabou o For");
+
+//while
+// var it = 1 
+// while(it < 10){
+//     console.log("Estou aqui 2-0 retorno");
+//     it++
+// }
+
+//função
+// function teste1(){
+//     console.log("Estou aqui na função 1");
+// }
+
+// function teste2(){
+//     console.log("Estou aqui na função 2");
+// }
+
+// teste1()
+
